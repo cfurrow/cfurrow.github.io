7dde31b37242643fc8004bdc20352ee205fe7c45
Carl Furrow <carl.furrow@gmail.com>
2015-01-20 21:34:20 -0500

    Change random func

--- assets/themes/conleche/scripts/hero_picker.js
+++ assets/themes/conleche/scripts/hero_picker.js
@@ -1,10 +1,15 @@
 $(function(){
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
   var $hero = $('#hero');
   var imageCount = 5;
   var path = "/assets/hero/";
 
-  var heroPath = "url(" + path + Math.floor((Math.random() * (imageCount + 1))).toString() + ".jpg)";
+  var heroPath = "url(" + path + getRandomArbitrary(1, imageCount+1).toString() + ".jpg)";
 
   $hero.css('background-image', heroPath);
+
 
 });
