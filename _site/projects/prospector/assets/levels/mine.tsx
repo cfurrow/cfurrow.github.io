4e0e15e7fc01e69e10c79ec2ce6696f438efe0b4
Carl Furrow <hello@carlfurrow.com>
2019-02-24 14:32:47 -0500

    Revert "Add _site folder"
    This reverts commit 534e79e82080a368bad7274b1fd0635553e48b49.

--- _site/projects/prospector/assets/levels/mine.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mine" tilewidth="96" tileheight="96" tilecount="2" columns="1">
- <image source="../gold-mine.png" width="96" height="192"/>
-</tileset>
--- _site/projects/prospector/src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  gameWidth: 1200,
-  gameHeight: 800,
-  localStorageName: 'cf-prospector'
-}
